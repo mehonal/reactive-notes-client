41951e1560ed7d6cc98ef1a8cdc8f701ebc6c358
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-10-24 14:42:02 +0300

    added Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
   return (
     <>
-        Starting. 
+      <div className="container-fluid mt-3">
+          <a href="/" className="btn btn-primary mb-2">Working button</a>
+          <p>Starting.</p>
+      </div>
     </>
   )
 }
