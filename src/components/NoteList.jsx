73fe90066b8a6378cd98bf157d7153eeba714d61
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-10-27 20:33:40 +0300

    linked getting notes list with up-to-date express configuration

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -24,8 +24,8 @@ function NoteList() {
             (
                 <div className="row">
                     { notes.map((note, index)=> (
-                        <div key={note.id} className={`col-sm-4 ${ index % 2 === 0 ? 'bg-danger' : 'bg-success'} d-flex justify-content-center align-items-center`} style={{minHeight: "300px"}}>
-                            <Link to={`/notes/${note.id}`} style = {{color: "white", textDecoration: "none"}}>{note.title}</Link>
+                        <div key={note._id} className={`col-sm-4 ${ index % 2 === 0 ? 'bg-danger' : 'bg-success'} d-flex justify-content-center align-items-center`} style={{minHeight: "300px"}}>
+                            <Link to={`/notes/${note._id}`} style = {{color: "white", textDecoration: "none"}}>{note.title}</Link>
                         </div>
                     ))}
                 </div>
